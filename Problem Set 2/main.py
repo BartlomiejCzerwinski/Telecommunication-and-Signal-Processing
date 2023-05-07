01f5674a443ec9ca303330bbcba3f07eabbad588
BartlomiejCzerwinski <bartek2002@interia.pl>
2023-05-07 20:04:11 +0200

    uses 2 ports on sending message

--- Problem Set 2/main.py
+++ Problem Set 2/main.py
@@ -12,14 +12,17 @@ def send_huffman_dict(huffman_dict, host, port):
         s.sendall(json_dict.encode())
 
 def huffman_encoding(data):
-    # zliczanie występowania każdego znaku w wiadomośći
     freq = defaultdict(int)
     for char in data:
         freq[char] += 1
-    # tworzenie zbioru z każdym znakiem i jego częstością występowania
     heap = [[weight, [symbol, ""]] for symbol, weight in freq.items()]
     heapq.heapify(heap)
-    # łączenie węzłów drzewa huffmana i tworzenie kodów dla znaków
+
+    if len(freq) == 1:
+        symbol = next(iter(freq))
+        huffman_dict = {symbol: '0'}
+        return huffman_dict
+
     while len(heap) > 1:
         left = heapq.heappop(heap)
         right = heapq.heappop(heap)
@@ -30,6 +33,8 @@ def huffman_encoding(data):
         heapq.heappush(heap, [left[0] + right[0]] + left[1:] + right[1:])
 
     huffman_dict = dict(sorted(heapq.heappop(heap)[1:], key=lambda p: (len(p[-1]), p)))
+
+
     return huffman_dict
 
 def send_huffman_encoded_message(encoded_message, host, port):
@@ -59,11 +64,12 @@ def receive_huffman_encoded_message(address, port, huffman_dict):
             if data:
                 encoded_message = json.loads(data.decode())
                 try:
-                    with open("zakodowane.txt", "w") as file:
+                    with open("encoded.txt", "w") as file:
                         file.write(encoded_message[1:-1])
                         file.close()
                 except FileNotFoundError:
-                    print("a")
+                    print("file not found!")
+
                 decoded_message = huffman_decoding(encoded_message, huffman_dict)
                 return decoded_message
 
@@ -83,38 +89,31 @@ decoded_message = ''
 
 def receiveMessageWindow():
     global decoded_message
-    # adres i port do nasłuchiwania
-    address = '192.168.100.4'
+    address = '192.168.233.11'
     port = 16049
 
-    # odbieranie słownika huffmana
     huffman_dict = receive_huffman_dict(address, port)
     abc = huffman_dict
     print(abc)
     try:
-        with open("słownik", "w") as file:
+        with open("codebook", "w") as file:
             file.write(json.dumps(abc))
             file.close()
     except FileNotFoundError:
          print("a")
 
-    # odbieranie zakodowanej wiadomości
     decoded_message = receive_huffman_encoded_message(address, port, huffman_dict)
 
-    # utworzenie nowego okna
     new_window = tk.Toplevel(root)
     new_window.geometry("300x100")
 
-    # naglowek tekstu
-    text_box_label = tk.Label(new_window, text="Podaj nazwe pliku:", font=("Aerial", 8))
+    text_box_label = tk.Label(new_window, text="Enter file name:", font=("Aerial", 8))
     text_box_label.pack(pady=5)
 
-    #pole tekstowe
     text_box = tk.Entry(new_window, textvariable=text_var1, font=("Arial", 8))
     text_box.pack(pady=5)
 
-    # przycisk w nowym oknie
-    new_button = tk.Button(new_window, text="Zapisz odebrany plik", font=("Arial", 8) ,command=receiveMessage)
+    new_button = tk.Button(new_window, text="Save received file", font=("Arial", 8) ,command=receiveMessage)
     new_button.pack(pady=5)
 
 def sendMessageWindow():
@@ -122,13 +121,13 @@ def sendMessageWindow():
     new_window = tk.Toplevel(root)
     new_window.geometry("300x100")
 
-    text_box_label = tk.Label(new_window, text="Podaj nazwe pliku:", font=("Aerial", 8))
+    text_box_label = tk.Label(new_window, text="Enter file name:", font=("Aerial", 8))
     text_box_label.pack(pady=5)
 
     text_box = tk.Entry(new_window, textvariable=text_var2, font=("Arial", 8))
     text_box.pack(pady=5)
 
-    new_button = tk.Button(new_window, text="Wyślij plik", font=("Arial", 8), command=sendMessage)
+    new_button = tk.Button(new_window, text="Send file", font=("Arial", 8), command=sendMessage)
     new_button.pack(pady=5)
 
 root = tk.Tk()
@@ -138,46 +137,43 @@ root.geometry("600x200")
 text_var1 = tk.StringVar()
 text_var2 = tk.StringVar()
 
-send_button = tk.Button(root, text="Wysyłaj",font=("Arial", 8), command=sendMessageWindow, height=5, width=100)
+send_button = tk.Button(root, text="Send",font=("Arial", 8), command=sendMessageWindow, height=5, width=100)
 send_button.pack(pady=10,padx=30)
 
-receive_button = tk.Button(root, text="Odbieraj",font=("Arial", 8), command=receiveMessageWindow, height=5, width=100)
+receive_button = tk.Button(root, text="Receive",font=("Arial", 8), command=receiveMessageWindow, height=5, width=100)
 receive_button.pack(pady=10, padx=30)
 
 def sendMessage():
 
-    # odczytanie wiadomości z pliku
     filename = text_var2.get()
     try:
         with open(filename, "r") as file:
             text = file.read()
             file.close()
     except FileNotFoundError:
-        messagebox.showinfo("Error", "Plik nie istnieje")
+        messagebox.showinfo("Error", "File doesn't exist")
 
-    # tworzenie słownika i kodowanie wiadomości
     huffman_dict = huffman_encoding(text)
     encoded_message = "".join(huffman_dict[char] for char in text)
 
-    # wysłanie zakodowanej wiadomości
-    host = "192.168.100.4"
-    port = 16049
-    send_huffman_dict(huffman_dict, host, port)
-    send_huffman_encoded_message(json.dumps(encoded_message), host, port)
-    messagebox.showinfo("Wysłano wiadomość", "Pomyślnie wysłano wiadomość.")
+    host = "192.168.233.125"
+    port1 = 16034
+    port2 = 16035
+    send_huffman_dict(huffman_dict, host, port1)
+    send_huffman_encoded_message(json.dumps(encoded_message), host, port2)
+    messagebox.showinfo("Message sent", "Message successfully sent.")
 
 
 def receiveMessage():
     global decoded_message
 
-    # zapisanie wiadomości na dysku
     filename = text_var1.get()
     try:
         with open(filename, "w") as file:
             file.write(decoded_message)
             file.close()
     except FileNotFoundError:
-        messagebox.showinfo("Error", "Nie podano nazwy")
-    messagebox.showinfo("Odebrano wiadomość", "Pomyślnie odebrano wiadomość.")
+        messagebox.showinfo("Error", "Enter file name first")
+    messagebox.showinfo("Message received", "Message successfully received.")
 
 root.mainloop()
